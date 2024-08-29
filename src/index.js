dfbc4724a55b34d6178b5c2e369216c05cabf5b2
Marta Jurado <martaju29@gmail.com>
2024-08-29 06:47:31 +0200

    tailwind plugin

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import createPlugin from 'tailwindcss/plugin'
+import createPlugin from 'tailwindcss/plugin.js'
 import theme from './theme.js'
 
 /** @type {import('tailwindcss/types/config').PluginCreator} */
